067343d52ba5354e9ab3ff14c1260a8a99cabe35
Cesar Roman <cesardroman18@gmail.com>
2022-12-21 11:37:39 -0600

    feature: added recommendations, made small changes in webpack and index files

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,5 @@
 export { default as Home } from './Home';
 export { default as Navbar } from './Navbar';
 export { default as NotFound } from './NotFound';
+export { default as Recommendations } from './Recommendations';
 export { default as Resume } from './Resume';
--- webpack.dev.js
+++ webpack.dev.js
@@ -9,6 +9,7 @@ module.exports = merge(common, {
     static: {
       directory: path.join(__dirname, 'public'),
     },
+    historyApiFallback: true,
     compress: true,
     port: 3000,
     hot: true,
