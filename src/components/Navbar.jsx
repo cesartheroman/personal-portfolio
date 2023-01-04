e344fccffe83a46080b0be4b1aba1533516c160d
Cesar Roman <cesardroman18@gmail.com>
2023-01-04 15:28:10 -0600

    regfactor: re-institute navbar and editing styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,29 +3,27 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <>
-      <input type="checkbox" id="hamburgerInput" className="burgerShower" />
-      <label className="hamburgerMenu" htmlFor="hamburgerInput">
-        <nav className="navbar">
-          <ul>
-            <li className="navItem">
-              <Link to="/">HOME</Link>
-            </li>
-          </ul>
-          <ul className="links">
-            <li className="navItem">
-              <Link to="/projects">PROJECTS</Link>
-            </li>
-            <li className="navItem">
-              <Link to="/recommendations">RECOMMENDATIONS</Link>
-            </li>
-            <li className="navItem">
-              <Link to="/resume">RESUME</Link>
-            </li>
-          </ul>
-        </nav>
-      </label>
-    </>
+    <nav className="navbar">
+      <ul>
+        <li className="navItem">
+          <h1>
+            <Link to="/">Cesar Roman</Link>
+          </h1>
+        </li>
+
+        <li className="navItem">
+          <Link to="/projects">PROJECTS</Link>
+        </li>
+
+        <li className="navItem">
+          <Link to="/recommendations">RECOMMENDATIONS</Link>
+        </li>
+
+        <li className="navItem">
+          <Link to="/resume">RESUME</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
