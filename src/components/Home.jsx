b2f0ecedae1a9603d86f19f1ad4f733b6df344fe
Cesar Roman <cesardroman18@gmail.com>
2023-01-05 12:49:21 -0600

    refactor: edited styles and changed picture and intro order

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,13 +7,6 @@ const Home = () => {
   return (
     <>
       <section className="home">
-        <figure className="pictureContainer">
-          <img
-            src={profilePicture}
-            alt="Picture showing Cesar, smiling with crossed arms in a white button-down shirt against an orange background."
-          />
-        </figure>
-
         <article className="intro">
           <p className="name">
             {copy.aboutMe.greeting}
@@ -26,6 +19,13 @@ const Home = () => {
 
           <p>{copy.aboutMe.currentJob}</p>
         </article>
+
+        <figure className="pictureContainer">
+          <img
+            src={profilePicture}
+            alt="Picture showing Cesar, smiling with crossed arms in a white button-down shirt against an orange background."
+          />
+        </figure>
       </section>
 
       <ContactMe />
