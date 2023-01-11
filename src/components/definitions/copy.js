dc42aa5e4f3bef10789e37657156984be62349c3
Cesar Roman <cesardroman18@gmail.com>
2023-01-11 12:58:55 -0600

    refactor: fixed typos in copy

--- src/components/definitions/copy.js
+++ src/components/definitions/copy.js
@@ -9,32 +9,14 @@ const copy = {
   aboutMe: {
     title: `About Cesar`,
     bodyTitle: `Just the highlights`,
-    line1: `I started my coding journey at the end 2019 after four amazing
-                years excelling in B2B software sales at `,
-    line2: `. Though I enjoyed my time in sales, I realized that instead of
-                only selling the solution, I wanted to also build the solution.
-                So, I took some time to go on my own "radical sabbatical" to
-                relax, introspect, and see what direction I wanted to take in my
-                career.`,
-    line3: `After a ton of research, I decided to accelerate my learning by
-                joining a coding bootcamp called `,
-    line4: ` in NYC in mid 2020. Upon completion, I started my engineering
-                journey as a Customer Success Engineer at `,
-    line5: ` at the beginning of 2021, and left at the end of the year in
-                order to pursue full stack engineering positions.`,
+    line1: `I started my coding journey at the end of 2019 after four amazing years excelling in B2B software sales at `,
+    line2: `. Though I enjoyed my time in sales, I realized that instead of only selling the solution, I wanted to build the solution. So, I took some time to go on my own "radical sabbatical" to relax, introspect, and see what direction I wanted to take my career.`,
+    line3: `After a month or so of self-teaching through free online resources, I decided to accelerate my learning by joining a coding bootcamp called `,
+    line4: ` in NYC in mid 2020. Upon completion later that year, I started my engineering journey as a Customer Success Engineer at `,
+    line5: ` at the beginning of 2021, and left at the end of the year in order to pursue full stack engineering positions.`,
     line6: `In early 2022, I came across `,
-    line7: `, decided to apply, and was selected as one of 8 software
-                engineers out of 3,000+ applicants to be accepted into their
-                rigorous 6 month apprenticeship. While I formally received an
-                "Exceeds Expectations" designation at the end of the
-                apprenticeship, Twilio wasn't able to retain me due to the wider
-                macro-economic climate at the end of 2022. Now, as a free agent,
-                I'm excited to see where my next adventure takes me!`,
-    line8: `Outside of coding, you can catch me yelling at my TV out of pure
-                joy or anger while rooting for the Indianapolis Colts or the
-                Mexican National Football Team. Outside of my sports fandom, you
-                can catch me playing with my Australian Cattle/Shepherd mix dog,
-                Benji!`,
+    line7: `, decided to apply, and was selected as one of 8 software engineers out of 3,000+ applicants to be accepted into their rigorous 6 month apprenticeship. While I formally received an "Exceeds Expectations" designation at the end of the apprenticeship, Twilio wasn't able to retain me due to the wider macro-economic climate at the end of 2022. Now, as a free agent, I'm excited to see where my next adventure takes me!`,
+    line8: `Outside of coding, you can catch me yelling at my TV out of pure joy—but most likely out of frustration—while rooting for the Indianapolis Colts or the Mexican National Football Team. Outside of my sports fandom, you can catch me playing with my Australian Cattle and Shepherd mix dog, Benji!`,
     techIntro: `My favorite technologies include:`,
     techBody: [`React`, `TypeScript`, `Node`, `Express`, `Postgres`],
   },
